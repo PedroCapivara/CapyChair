85aeaf9633339f2423ef5a67944bdcba3eeb2ca1
PedroCapivara <152212470+PedroCapivara@users.noreply.github.com>
2025-04-23 23:09:03 -0300

    Finish

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,1 +1,13 @@
-let 
+document.addEventListener("DOMContentLoaded", () => {
+  const detalhes = document.querySelectorAll("details");
+
+  detalhes.forEach((detail) => {
+    detail.addEventListener("toggle", () => {
+      if (detail.open) {
+        detail.classList.add("expandido");
+      } else {
+        detail.classList.remove("expandido");
+      }
+    });
+  });
+});
